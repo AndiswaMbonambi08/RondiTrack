7520a104f66f19f71e7c1025a59c4ec9ca86e284
AndiswaMbonambi <andiswambonambi12@gmail.com>
2026-09-22 23:01:09 +0200

    Retrofit status codes across Assignment 4.1 endpoints

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
-//Wires up DI, OpenAPI/Scalar, maps the endpoint groups, seeds one stokvel with members on startup.
-using Microsoft.AspNetCore.OpenApi;
 using RondiTrack.Data;
 using RondiTrack.Domain;
 using RondiTrack.Endpoints;
+using RondiTrack.Services;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
 builder.Services.AddSingleton<IStokvelRepository, InMemoryStokvelRepository>();
+builder.Services.AddSingleton<IIdempotencyStore, InMemoryIdempotencyStore>();
+builder.Services.AddSingleton<IStokvelService, StokvelService>();
 
 var app = builder.Build();
 
@@ -29,11 +30,10 @@ using (var scope = app.Services.CreateScope())
 
     var users = await userRepo.GetAllAsync();
     var stokvel = new Stokvel("Ubuntu Savings Circle", 500m);
-        
-    foreach (var user in users.Where(u => u.IsActive))
-    {
-        stokvel.AddMember(user);
-    }
+    stokvel.AddMember(users[0]);
+    stokvel.AddMember(users[1]);
     await stokvelRepo.AddAsync(stokvel);
 }
+
 app.Run();
+/*so what we were basically doing for this is assignment is tis....the changes i made in each file and while i doid it the way i did it, why i chose the classes and functions i chose...fuction oof all of them...must exaplain in human terms for me to actually understand what is it we were doing what was the point*/
